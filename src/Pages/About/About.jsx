bc48b51a2d356f753a37e1b1072ae1f136412f34
Abdirino <abdirinoahash@gmail.com>
2023-07-20 10:46:42 +0300

    about sec 2/3

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -6,6 +6,8 @@ import Footer from "../../Components/Footer/Footer";
 import aboutIMG from "../../Images/IAffordable-About.jpg";
 import Reason from "../../Components/Reason/Reason";
 
+import Person1 from "../../Images/our1.jpg";
+
 const About = () => {
   return (
     <>
@@ -13,19 +15,22 @@ const About = () => {
       <div className="about">
         <div className="About">
           <div className="about-detail">
+            <h2>About Us</h2>
             <h1>
-              Who <span>We Are</span>
+              We are <span>no. 1 internet service provider</span> company in
+              Kenya.
             </h1>
-            <h3>
-              🔷 We are the largest Internet Service Provider in the market
-            </h3>
             <p>
               We’re committed to making Internet simple for all clients to
-              connect across both our own network and the National Network. Our
-              vision is to lead the market wit services that harness the
-              potential of the Internet and then differentiate with an ning
-              customer service. We employ more than 2,500 enthusiastic.
+              connect across our own network. Our vision is to lead the market
+              with services that harness the potential of the Internet.
             </p>
+            <ul>
+              <li>Free Installation</li>
+              <li>Internet Service Solutions</li>
+              <li>Top Quality Service With Affordable Pricing</li>
+              <li>Live Video Streaming With Ultra Fast Speed</li>
+            </ul>
           </div>
           <div className="about-image">
             <img src={aboutIMG} alt="" />
@@ -33,7 +38,26 @@ const About = () => {
         </div>
       </div>
       <Reason />
-      <div className="team"></div>
+      <div className="team">
+        <h1>
+          Meet <span>Our Team</span>
+        </h1>
+        <p>
+          "Our team is a dedicated and skilled group of professionals committed
+          to delivering reliable and high-speed internet solutions, ensuring
+          seamless connectivity for all your online needs."
+        </p>
+      </div>
+      <div className="video-tour">
+        <h1>
+          Video <span>Tour</span>
+        </h1>
+        <p>
+          "Discover how iAffordable, your trusted internet service provider,
+          offers unbeatable value and exceptional connectivity in our latest
+          video showcasing our top-notch services and satisfied customers."
+        </p>
+      </div>
       <Footer />
     </>
   );
