e32c15dcbf0ef296969bb8341f32a0754a8eb6cd
Abdirino <abdirinoahash@gmail.com>
2023-07-22 11:56:12 +0300

    TO BE

--- src/Components/Quizzes/Quizzes.jsx
+++ src/Components/Quizzes/Quizzes.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./quizzes.css";
 
 const Quizzes = () => {
+  
   return (
     <>
       <div className="quizzes">
@@ -13,62 +14,6 @@ const Quizzes = () => {
         </p>
         <div className="quiz">
           <div className="accordion">
-
-            <div className="accordion-content">
-              <header>
-                <span className="title">How to reset a Tenda Router?</span>
-                <i class="fa-solid fa-plus"></i>
-              </header>
-
-              <p className="description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-                eius veniam voluptatibus obcaecati asperiores cum et molestias
-                praesentium temporibus tempore amet vero ullam, quod adipisci
-                accusantium magnam cupiditate delectus vitae.
-              </p>
-            </div>
-
-            <div className="accordion-content">
-              <header>
-                <span className="title">How to reset a Tenda Router?</span>
-                <i class="fa-solid fa-plus"></i>
-              </header>
-
-              <p className="description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-                eius veniam voluptatibus obcaecati asperiores cum et molestias
-                praesentium temporibus tempore amet vero ullam, quod adipisci
-                accusantium magnam cupiditate delectus vitae.
-              </p>
-            </div>
-
-            <div className="accordion-content">
-              <header>
-                <span className="title">How to reset a Tenda Router?</span>
-                <i class="fa-solid fa-plus"></i>
-              </header>
-
-              <p className="description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-                eius veniam voluptatibus obcaecati asperiores cum et molestias
-                praesentium temporibus tempore amet vero ullam, quod adipisci
-                accusantium magnam cupiditate delectus vitae.
-              </p>
-            </div>
-            
-            <div className="accordion-content">
-              <header>
-                <span className="title">How to reset a Tenda Router?</span>
-                <i class="fa-solid fa-plus"></i>
-              </header>
-
-              <p className="description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-                eius veniam voluptatibus obcaecati asperiores cum et molestias
-                praesentium temporibus tempore amet vero ullam, quod adipisci
-                accusantium magnam cupiditate delectus vitae.
-              </p>
-            </div>
 
           </div>
         </div>
