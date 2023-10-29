791fe7690bac0f05d834c7e6fcbba7335cd5e28d
Abdirahaman Abdulaziz <abdirinoahash@gmail.com>
2023-10-29 18:02:58 +0300

    perfomance boost

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { Suspense, lazy, useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./Pages/Home/Home";
-import Packages from "./Pages/Packages/Packages";
-import About from "./Pages/About/About";
 import ScrollToTop from "./ScrollToTop";
-import Contact from "./Pages/Contact/Contact";
-import Service from "./Pages/Services/Service";
-
 import Loader from "./Components/Loader/Loader";
-import Service1 from "./Pages/ServicesInfo/Service1";
-import Service2 from "./Pages/ServicesInfo/Service2";
-import Service3 from "./Pages/ServicesInfo/Service3";
-import FAQs from "./Components/FAQs/FAQs";
+
+const Home = lazy(() => import("./Pages/Home/Home")); 
+const About = lazy(() => import("./Pages/About/About"));
+const Service = lazy(() => import("./Pages/Services/Service"));
+const FAQs = lazy(() => import("./Components/FAQs/FAQs"));
+const Contact = lazy(() => import("./Pages/Contact/Contact"));
+const Packages = lazy(() => import("./Pages/Packages/Packages"));
+
+const Service1 = lazy(() => import("./Pages/ServicesInfo/Service1"));
+const Service2 = lazy(() => import("./Pages/ServicesInfo/Service2"));
+const Service3 = lazy(() => import("./Pages/ServicesInfo/Service3"));
 
 function App() {
   const [loading, setLoading] = useState(false);
@@ -29,18 +30,20 @@ function App() {
       ) : (
         <BrowserRouter>
           <ScrollToTop />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/service" element={<Service />} />
-            <Route path="/package" element={<Packages />} />
-            <Route path="/contact" element={<Contact />} />
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/service" element={<Service />} />
+              <Route path="/package" element={<Packages />} />
+              <Route path="/contact" element={<Contact />} />
 
-            <Route path="/service1" element={<Service1 />} />
-            <Route path="/service2" element={<Service2 />} />
-            <Route path="/service3" element={<Service3 />} />
-            <Route path="/FAQs" element={<FAQs />} />
-          </Routes>
+              <Route path="/service1" element={<Service1 />} />
+              <Route path="/service2" element={<Service2 />} />
+              <Route path="/service3" element={<Service3 />} />
+              <Route path="/FAQs" element={<FAQs />} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       )}
     </div>
