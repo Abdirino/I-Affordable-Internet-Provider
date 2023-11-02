261db6ff495b841df16a438050167994f5e296ef
Abdirahaman Abdulaziz <abdirinoahash@gmail.com>
2023-11-02 08:33:08 +0300

    react-countup add ✅

--- src/Components/Reason/Reason.jsx
+++ src/Components/Reason/Reason.jsx
@@ -1,37 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import "./reason.css";
 import Reveal from "../Reveal/Reveal";
+import CountUp from "react-countup";
+import ScrollTrigger from "react-scroll-trigger";
 
 const Reason = () => {
+  const [counter, setCounter] = useState(false);
+
   return (
     <>
-      <div className="reasons">
-        <div className="head">
-          <Reveal>
-            <h1>
-              Why <span>Embrace</span> Our Network?
-            </h1>
-          </Reveal>
-        </div>
+      <ScrollTrigger
+        onEnter={() => setCounter(true)}
+        onExit={() => setCounter(false)}
+      >
+        <div className="reasons">
+          <div className="head">
+            <Reveal>
+              <h1>
+                Why <span>Embrace</span> Our Network?
+              </h1>
+            </Reveal>
+          </div>
           <div className="reason">
             <div className="re">
-              <h2>500+</h2>
+              <h2>
+                {counter && (
+                  <CountUp start={0} end={500} delay={0} duration={2} />
+                )}
+                +
+              </h2>
               <p>Happy Customers</p>
             </div>
             <div className="re">
-              <h2>10+</h2>
+              <h2>
+                {counter && (
+                  <CountUp start={0} end={12} delay={0} duration={2} />
+                )}
+                +
+              </h2>
               <p>Support Services</p>
             </div>
             <div className="re">
-              <h2>30+</h2>
+              <h2>
+                {counter && (
+                  <CountUp start={0} end={30} delay={0} duration={2} />
+                )}
+                +
+              </h2>
               <p>Access Points</p>
             </div>
             <div className="re">
-              <h2>12/7</h2>
+              <h2>
+                12/7
+              </h2>
               <p>Customer Service</p>
             </div>
           </div>
-      </div>
+        </div>
+      </ScrollTrigger>
     </>
   );
 };
