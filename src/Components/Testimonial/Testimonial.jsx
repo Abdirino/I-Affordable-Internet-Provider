90f9a7a505d22ae0078dc53d948191703ab5dc5d
Abdirahaman Abdulaziz <abdirinoahash@gmail.com>
2024-01-10 19:39:34 +0300

    testimonial section start

---
+++ src/Components/Testimonial/Testimonial.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Testimonial.css"
+
+const Testimonial = () => {
+  return (
+    <div className='testimonial'>
+        <div className="test-content">
+            <div className="slide"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Testimonial
